01b78ebfe97d202888ca6a24fda22cbece0968bd
MagnusThor <magnus.thor74@gmail.com>
2021-09-06 19:44:42 +0200

    dep.  updates

--- example/shaders/bufferA_frag.js
+++ example/shaders/bufferA_frag.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUFFER_A_FRAG = void 0;
-exports.BUFFER_A_FRAG = "uniform float time;\nuniform vec2 mouse;\nuniform vec2 resolution;\n\nuniform sampler2D iChannel0;\nuniform sampler2D iChannel1;\nuniform sampler2D iChannel2;\nuniform sampler2D iChannel3;\nuniform sampler2D iChannel4;\n\nout vec4 fragColor;\n\n\nvoid main(){\n    \n  vec2 uv=(-resolution.xy+2.*gl_FragCoord.xy)/resolution.y;\n  vec4 t = texture(iChannel1,uv);\n  fragColor = t; // vec4(vec3(1.,0.,0.1),1.);\n    \n}\n\n";
+exports.BUFFER_A_FRAG = "uniform float time;\nuniform vec2 mouse;\nuniform vec2 resolution;\nuniform sampler2D iChannel0;\nuniform sampler2D iChannel1;\nuniform sampler2D iChannel2;\nuniform sampler2D iChannel3;\nuniform sampler2D iChannel4;\n\nout vec4 fragColor;\n\nvoid main(){    \n  vec2 uv=(-resolution.xy+2.*gl_FragCoord.xy)/resolution.y;\n  vec4 t = texture(iChannel1,uv);\n  fragColor = t; // vec4(vec3(1.,0.,0.1),1.);\n}";
--- example/shaders/bufferA_frag.ts
+++ example/shaders/bufferA_frag.ts
@@ -1,7 +1,6 @@
 export const BUFFER_A_FRAG = `uniform float time;
 uniform vec2 mouse;
 uniform vec2 resolution;
-
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
 uniform sampler2D iChannel2;
@@ -10,14 +9,9 @@ uniform sampler2D iChannel4;
 
 out vec4 fragColor;
 
-
-void main(){
-    
+void main(){    
   vec2 uv=(-resolution.xy+2.*gl_FragCoord.xy)/resolution.y;
   vec4 t = texture(iChannel1,uv);
   fragColor = t; // vec4(vec3(1.,0.,0.1),1.);
-    
-}
-
-`;
+}`;
 
